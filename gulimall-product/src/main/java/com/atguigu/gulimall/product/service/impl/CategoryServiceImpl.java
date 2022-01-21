1e73868418ec9b8758fbc29c7ec4610ebbde189d
jonil <1158984345@qq.com>
2022-01-21 23:26:03 +0800

    三级分类菜单完成

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,16 +1,20 @@
 package com.atguigu.gulimall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.Query;
-
 import com.atguigu.gulimall.product.dao.CategoryDao;
 import com.atguigu.gulimall.product.entity.CategoryEntity;
 import com.atguigu.gulimall.product.service.CategoryService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 
 @Service("categoryService")
@@ -20,10 +24,48 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryEntity> page = this.page(
                 new Query<CategoryEntity>().getPage(params),
-                new QueryWrapper<CategoryEntity>()
+                new QueryWrapper<>()
         );
 
         return new PageUtils(page);
     }
 
+    /**
+     * 树形结构返回菜单列表
+     * @return 菜单列表
+     */
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1、查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        //2、组装成父子的树形结构
+        List<CategoryEntity> levelMenus = entities.stream().filter(categoryEntity -> categoryEntity.getParentCid() == 0).map(menu -> {
+            menu.setChildren(getChildren(menu, entities));
+            return menu;
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))).collect(Collectors.toList());
+        return levelMenus;
+    }
+
+    /**
+     * 递归查找所有菜单的子菜单
+     *
+     * @param root 当前菜单
+     * @param menu  所有菜单
+     * @return 子菜单
+     */
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> menu) {
+        //1、判断是否相等
+        //2、相等就用peek函数操作，然后在peek函数操作中递归使用getChildren()函数，形成父子树形结构
+        //3、排序
+        return menu.stream().filter(categoryEntity -> Objects.equals(categoryEntity.getParentCid(), root.getCatId()))
+                .peek(categoryEntity -> categoryEntity.setChildren(getChildren(categoryEntity, menu)))
+                .sorted(Comparator.comparingInt(menu1 -> (menu1.getSort() == null ? 0 : menu1.getSort()))).collect(Collectors.toList());
+    }
+
+    @Override
+    public void removeMenuByIds(List<Long> asList) {
+        //TODO 检查当前删除的菜单，是否被别的地方引用
+
+        baseMapper.deleteBatchIds(asList);
+    }
 }
