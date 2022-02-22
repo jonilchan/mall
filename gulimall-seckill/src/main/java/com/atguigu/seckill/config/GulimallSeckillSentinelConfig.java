963bad156fe5865f63f552d6dcfe66009b4e94ea
jonil <1158984345@qq.com>
2022-02-22 22:48:36 +0800

    订单服务完成

---
+++ gulimall-seckill/src/main/java/com/atguigu/seckill/config/GulimallSeckillSentinelConfig.java
@@ -0,0 +1,30 @@
+//package com.atguigu.seckill.config;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.atguigu.common.exception.BizCodeEnum;
+//import com.atguigu.common.utils.R;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@Configuration
+//public class GulimallSeckillSentinelConfig {
+//
+//    public GulimallSeckillSentinelConfig() {
+//
+//        WebCallbackManager.setUrlBlockHandler(new UrlBlockHandler() {
+//            @Override
+//            public void blocked(HttpServletRequest request, HttpServletResponse response, BlockException ex) throws IOException {
+//                R error = R.error(BizCodeEnum.TOO_MANY_REQUEST.getCode(), BizCodeEnum.TOO_MANY_REQUEST.getMsg());
+//                response.setCharacterEncoding("UTF-8");
+//                response.setContentType("application/json");
+//                response.getWriter().write(JSON.toJSONString(error));
+//
+//            }
+//        });
+//
+//    }
+//
+//}
